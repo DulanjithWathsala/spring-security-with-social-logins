ae02a7ee18034a07e32c0726a2b7061af4cc8425
M.D.W.De Silva <wdulanjith@gmail.com>
2024-10-01 14:41:19 +0530

    first commit

---
+++ src/main/java/edu/eazybytes/springSecOAUTH2/SpringSecOauth2Application.java
@@ -0,0 +1,13 @@
+package edu.eazybytes.springSecOAUTH2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecOauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecOauth2Application.class, args);
+	}
+
+}
---
+++ src/test/java/edu/eazybytes/springSecOAUTH2/SpringSecOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eazybytes.springSecOAUTH2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecOauth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
